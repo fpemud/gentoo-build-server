a6dfba03c8249a5bcd383788728bd66318b225fa
fpemud <fpemud@sina.com>
2016-09-21 10:19:32 +0800

    fi

---
+++ lib/gbs_ctrl_server.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+
+
+class GbsCtrlServer:
+
+    def __init__(self, param, acceptCallback, requestCallback):
+        self.param = param
+        self.acceptCallback = acceptCallback
+        self.requestCallback = requestCallback
+
+        self.serverSocket = None
+        self.serverSocketSourceId = None
+        self.handshaker = None
+        self.sessionDict = dict()
+
+    def 
+
+
+    def start(self):
+        self.serverSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serverSock.bind(('0.0.0.0', port))
+        self.serverSock.listen(5)
+        self.serverSock.setblocking(0)
+        self.serverSourceId = GLib.io_add_watch(self.serverSock, GLib.IO_IN | _flagError, self._onServerAccept)
+        self.handshaker = _HandShaker(self.param.certFile, self.param.privkeyFile, self.param.certFile, self._onHandShakeComplete, self._onHandShakeError)
+
+    def _onServerAccept(self, source, cb_condition):
+        assert not (cb_condition & _flagError)
+        assert source == self.serverSock
+
+        try:
+            new_sock, addr = self.serverSock.accept()
+            self.handshaker.addSocket(new_sock, True)
+            logging.info("Control Server: Client \"%s\" accepted." % (addr))
+            return True
+        except socket.error as e:
+            logging.error("Control Server: Client accept failed, %s, %s", e.__class__, e)
+            return True
+
+    def _onHandShakeComplete(self, source, sslSock, hostname, port):
+        logging.info("Control Server: Client \"%s\" hand shake complete." % (sslSock.getpeername()))
+        obj = GbsCtrlSession()
+        obj.recvBuf = ""
+        obj.recvSourceId = GLib.io_add_watch(sslSock, GLib.IO_IN | _flagError, self._onRecv)
+        obj.privateData = self.acceptCallback(sslSock.get_peer_certificate().get_pubkey())
+        self.sessionDict[sslSock] = obj
+
+    def _onHandShakeError(self, source, hostname, port):
+        logging.error("Control Server: Client \"%s\" hand shake error." % (source))
+        source.close()
+
+    def _onRecv(self, source, cb_condition):
+        sessObj = self.sessionDict[source]
+
+        # receive from peer
+        buf = source.recv(4096)
+
+        # peer disconnects
+        if len(buf) == 0:
+            del self.sessionDict[source]
+            GLib.source_remove(sessObj.recvSourceId)
+            source.close()
+            return False
+
+        # save data to receive buffer
+        sessObj.recvBuf += buf
+
+        # we have received a json object, which must be a request
+        i = sessObj.recvBuf.find("\n")
+        if i >= 0:
+            requestObj = json.loads(sessObj.recvBuf[:i])
+            sessObj.recvBuf = sessObj.recvBuf[i+1:]
+            responseObj = self.requestCallback(requestObj)
+            source.send(json.dumps(responseObj))
+
+        return True
+
+
+class GbsCtrlSession:
+
+    def __init__(self):
+        self.recvBuf = None
+        self.recvSourceId = None
+        self.privateData = None
+
